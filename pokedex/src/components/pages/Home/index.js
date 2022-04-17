226abd14cb36e9a8014ffc1257c52ba85929561f
wellington <wellingtonmarcelinosotero@hotmail.com>
2022-04-17 17:54:23 -0300

    well-sotero

--- pokedex/src/components/pages/Home/index.js
+++ pokedex/src/components/pages/Home/index.js
@@ -1,6 +1,27 @@
 import React from "react";
 
 const Home = () => {
-    return(<>Home page</>)
-}
+  const pokemons = Home ();
+  return (
+    <div>
+      <h1>Sejam todos bem vindo ao Pokedex!</h1>
+      <h5>Qual a boa de hoje?</h5>
+
+      <button
+        onClick={() => {
+          
+        }}
+      >
+        Ir para Pokedex
+      </button>
+      <button
+        onClick={() => {
+         
+        }}
+      >
+        Destaques dos Pokémons
+      </button>
+    </div>
+  );
+};
 export default Home;
