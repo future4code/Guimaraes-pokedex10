2364c5e96319ce970fe12be7766476620e20084c
Miguel Tocchetto <miguelpereira.dev@gmail.com>
2022-04-18 00:27:42 -0300

    Routes e Navigation criados; Adiconado o Header às páginas

---
+++ pokedex/src/components/pages/Home/Home.js
@@ -0,0 +1,80 @@
+import React from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { ButtonTextFieldLabel } from "../../shared/ButtonTextField";
+import {goToPokedex, goToPokemonDetails, goBack} from '../../Navigation/Navigation'
+import styled from "styled-components"
+
+const Header = styled.div`
+    grid-area: HE;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: #1db954;
+    /* border: 1px solid black; */
+    background-color: black;
+
+    // :hover{
+    //     color: white;
+    // }
+
+
+    .header-left{
+        display: flex;
+        justify-content: start;
+        align-items: center;
+        /* border: 1px solid black; */
+        width: 250px;
+        height: 100%;
+        margin-left: 50px;
+    }
+
+    .header-right{
+        display: flex;
+        justify-content: end;
+        align-items: center;
+        /* border: 1px solid black; */
+        width: 250px;
+        height: 100%;
+        margin-right: 50px;
+    }
+`;
+
+const Home = () => {
+    const navigate = useNavigate();
+
+    return(
+        <Header>
+        <div className="header-left">
+            <div>
+                <ButtonTextFieldLabel 
+                    label={'Voltar'} 
+                    height={'30px'}
+                    radius={'30px'}
+                    backgroundColor={'#1db954'}
+                    onClick={()=>{goBack(navigate)}}
+                />
+            </div>
+        </div>
+
+        <div className="header-center">
+            <div>Nome do Pokemon</div>
+        </div>
+
+        <div  className="header-right">
+            <div>
+                <ButtonTextFieldLabel 
+                    label={'Pokedex'} 
+                    height={'30px'}
+                    width={'210px'}
+                    radius={'30px'}
+                    backgroundColor={'#1db954'}
+                    onClick={()=>{goToPokedex(navigate)}}
+                />
+            </div>
+        </div>
+        
+    </Header>
+    )
+}
+export default Home; 
--- pokedex/src/components/shared/ButtonTextField/index.js
+++ pokedex/src/components/shared/ButtonTextField/index.js
@@ -4,7 +4,7 @@ import * as S from './styles'
 
 
 const ButtonTextFieldLabel = ({ label, width, height, radius, backgroundColor }) => {
-    return(<S.Button width={width} height={height} radius={radius} backgroundColor={backgroundColor} >
+    return(<S.Button width={width} height={height} radius={radius} backgroundColor={backgroundColor}>
             {label}
     </S.Button>)
 }
